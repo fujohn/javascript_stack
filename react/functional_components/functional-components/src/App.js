acd3b32f17a1c9f51d21e79a0078865c7f2dfeb6
John Fu <fu.johnyh@gmail.com>
2023-01-11 18:09:14 -0800

    .

---
+++ react/functional_components/functional-components/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Person from './components/Person'
+
+function App() {
+  return (
+    <div className="App">
+      <Person />
+      
+      <Person />
+    </div>
+  );
+}
+
+export default App;
