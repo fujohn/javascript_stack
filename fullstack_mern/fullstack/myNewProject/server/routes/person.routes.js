e41727b5209755f84c4722c7aa6c6a89d241812e
John Fu <fu.johnyh@gmail.com>
2023-02-10 22:11:18 -0800

    beginning of product manager assignment

--- fullstack_mern/fullstack/myNewProject/server/routes/person.routes.js
+++ fullstack_mern/fullstack/myNewProject/server/routes/person.routes.js
@@ -1,5 +1,7 @@
-const PersonController = require('../controllers/person.controller');  //Import the code from Code Block 1
+const PersonController = require('../controllers/person.controller');
+
 module.exports = (app) => {
     app.get('/api', PersonController.index);
+    app.post('/api/people', PersonController.createPerson);     /* This is new */
 }
 
--- fullstack_mern/fullstack/myNewProject/server/server.js
+++ fullstack_mern/fullstack/myNewProject/server/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const cors = require('cors');    /* This is new */
+const cors = require('cors');
 
 const app = express();
-app.use(cors());                 /* This is new */
 
-/* This is a short-hand notation we use: */
+app.use(cors());
+app.use(express.json());                           /* This is new and allows JSON Objects to be posted */
+app.use(express.urlencoded({ extended: true }));   /* This is new and allows JSON Objects with strings and arrays*/
+
+require('./config/mongoose.config');    /* This is new */
 require('./routes/person.routes')(app);
 
-/* These two lines are the long-hand notation of the above code, to better illustrate what's going on: */
-/* const personRoutes = require("./routes/person.routes"); */
-/* personRoutes(app); */
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
-});
+})
 
