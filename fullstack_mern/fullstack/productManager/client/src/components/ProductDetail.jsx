0e30e55ac1e86b6cca47c41b54364ec1626ab3f5
John Fu <fu.johnyh@gmail.com>
2023-02-14 20:39:28 -0800

    PM Part 3

--- fullstack_mern/fullstack/productManager/client/src/components/ProductDetail.jsx
+++ fullstack_mern/fullstack/productManager/client/src/components/ProductDetail.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 const ProductDetail = (props) => {
     const [product, setProduct] = useState({})
     const {id} = useParams(); 
+    const navigate = useNavigate();
+
     useEffect(() => {
         axios.get("http://localhost:8000/api/products/" + id)
             .then( res => {
@@ -13,11 +15,22 @@ const ProductDetail = (props) => {
             })
             .catch( err => console.log(err) );
     }, []);
+    
+
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products/' + productId)
+            .then(res => {
+                navigate("/"); // this will take us back to the Main.js
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div>
             <h2>{product.title}</h2>
             <p>Price: ${product.price}</p>
             <p>Description: {product.description}</p>
+            <button onClick={ (e) => {deleteProduct(product._id)} }>Delete</button>
         </div>
     );
 }
